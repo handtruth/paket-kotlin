d67c9485b9b3b9829711013f6d89bec30aad9934
ktlo <ktlo@handtruth.com>
2019-08-19 05:07:31 +0300

    Gradle Kotlin Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,44 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    base
+    kotlin("jvm")
+    jacoco
 }
 
 group = "com.handtruth"
 version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.1")
+    testImplementation(kotlin("test"))
+}
+
+jacoco {
+    toolVersion = "0.8.4"
+    reportsDir = file("$buildDir/customJacocoReportDir")
+}
+
+tasks{
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = false
+            csv.isEnabled = false
+            html.destination = file("$buildDir/jacocoHtml")
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,13 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+                useVersion(gradle.rootProject.extra["kotlin.version"] as String)
+            }
+        }
+    }
 }
 
-rootProject.name = "gradle-wrapper"
+rootProject.name = "gradle-kotlin"
